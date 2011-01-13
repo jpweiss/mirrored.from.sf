6f8af7e9c1e7bae8eec0432c7e969bc6aef2d7e8
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2011-01-13 23:55:13 +0000

    - Adding a reset-to-my-tpX40-bindings script - Removed the mousekeys:ctrl_shift - Added mousekeys:ctrl_alt - Replaced 'purge-files.sh' with 'clean.sh' - Replaced bogus type keywords
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/xkb/trunk@2100 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

---
+++ tpX40/tests/reset2tpX40.sh
@@ -0,0 +1,7 @@
+setxkbmap -v -option
+setxkbmap -v -rules thinkpadx40 -model thinkpadx40 \
+    -option altwin:fn_super \
+    -option numlk:invert #\
+#    -option numlk:un_ctrl #\
+#    -option mousekeys:alt
+setxkbmap -v
--- tpX40/tests/verify-tpX40.sh
+++ tpX40/tests/verify-tpX40.sh
@@ -1,4 +1,4 @@
-#!/bin/sh
+#!/bin/bash
 
 
 # The default test options.
