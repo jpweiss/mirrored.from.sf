de6252bae99dc5c15dc271b5df095e314c0f09b3
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2011-06-07 19:51:34 +0000

    - Wireless-Toggle...   + ...now has the same format as my boilerplate;   + ...has separate functions for bringing the wifi up, turning it     off, toggling, and checking a specific wifi device.   + ...redirects all output from the functions to the $LOGFILE.   + ...tries using 'iwconfig', then tries modifying the sysfs files;   + ...calls 'toggleAllWirelessStates' when all else fails. - Whitespace cleanup. - Added kernel-version-specific control of wifi module loading &   unloading.
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/sh.scripts/trunk@2189 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

--- laptop/acpi/jpw.actions/ibm-wireless.sh
+++ laptop/acpi/jpw.actions/ibm-wireless.sh
@@ -1,19 +1,84 @@
 #!/bin/sh
+#
+# Find and toggle wireless of bluetooth devices on ThinkPads
+#
+# Copyright (C) 2011 by John P. Weiss
+#
+# This package is free software; you can redistribute it and/or modify
+# it under the terms of the Artistic License, included as the file
+# "LICENSE" in the source code archive.
+#
+# This package is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+#
+# You should have received a copy of the file "LICENSE", containing
+# the License John Weiss originally placed this program under.
+#
+# $Id$
+############
+
+
+############
+#
+# Configuration Variables
+#
+############
+
+
+WIFI_DEV=eth1
+
+
+############
+#
+# Includes & Other Global Variables
+#
+############
+
 
 test -f /usr/share/acpi-support/state-funcs || exit 0
-
-# Find and toggle wireless of bluetooth devices on ThinkPads
-
 . /usr/share/acpi-support/state-funcs
+
 . /etc/LocalSys/init.d/functions-laptop
 LOGFILE=/tmp/wifi-acpi.log
 
 
+############
+#
+# Functions
+#
+############
+
+
+isWifiUp()
+{
+    # Set the default.
+    local isOn=0
+    isAnyWirelessPoweredOn && isOn=1
+
+    local isUp=1
+    local opStF="/sys/class/net/${WIFI_DEV}/device/rf_kill"
+    if [ -r $opStF ]; then
+        isUp="`cat $opStF`"
+    else
+        isUp=$isOn
+    fi
+
+    [ "$isOn$isUp" = "00" ] && return 1
+    [ "$isOn$isUp" = "11" ] && return 0
+    # else:  Ambiguous result.  Return a slightly different code.
+    return 10
+}
+
+
 toggleBluetooth()
 {
-    [ -e /tmp/Fn-F5.ctrl-bluetooth ] || return 0
+    if [ ! -e /tmp/Fn-F5.ctrl-bluetooth ]; then
+        echo "(Bluetooth toggling suppressed.  No action taken.)"
+        return 0
+    fi
 
-    echo "Checking Bluetooth..." >>$LOGFILE
+    echo "Checking Bluetooth..."
 
     rfkill list |\
         sed -n -e'/tpacpi_bluetooth_sw/,/^[0-9]/p' |\
@@ -24,15 +89,129 @@ toggleBluetooth()
     if ! isAnyWirelessPoweredOn; then
         # Wireless was turned off
         if [ "$bluetooth_state" = 0 ]; then
-            echo "Bluetooth currently turned off; reenabling..." >>$LOGFILE
-            rfkill unblock bluetooth >>$LOGFILE 2>&1
+            echo "Bluetooth currently turned off; reenabling..."
+            rfkill unblock bluetooth
         else
-            echo "Disabling Bluetooth..." >>$LOGFILE
-            rfkill block bluetooth >>$LOGFILE 2>&1
+            echo "Disabling Bluetooth..."
+            rfkill block bluetooth
             #killBluetooth
         fi
     fi
 }
+
+
+turnWifiOff()
+{
+    local iwconfig_failed rfk
+    iwconfig_failed='n'
+
+    echo "Found active WiFi; disabling..."
+    iwconfig $WIFI_DEV txpower off || iwconfig_failed='y'
+    if [ "$iwconfig_failed" != "y" ]; then
+        isWifiUp
+        # If the wifi isn't unambiguously down, then iwconfig failed to
+        # work.
+        if [ $? -ne 0 ]; then
+            iwconfig_failed='y'
+        fi
+    fi
+
+    if [ "$iwconfig_failed" = "y" ]; then
+        echo "    'iwconfig' failed to disable TX power!"
+        echo "    Attempting direct kernel param manipulation"
+
+        killWifi
+    fi
+
+    # Return an appropriate status, depending on whether or not we failed to
+    # unambiguously turn the antenna off.
+    isWifiUp 
+    if [ $? -ne 0 ]; then
+        return 1
+    fi
+    #else
+    return 0
+}
+
+
+turnWifiOn()
+{
+    local iwconfig_failed
+    iwconfig_failed='n'
+
+    echo "Enabling WiFi..."
+    loadWifiModules
+
+    iwconfig ${WIFI_DEV} txpower on || iwconfig_failed='y'
+    if [ "$iwconfig_failed" != "y" ]; then
+        isWifiUp
+        # If the wifi isn't unambiguously up, then iwconfig failed to
+        # work.
+        if [ $? -ne 1 ]; then
+            iwconfig_failed='y'
+        fi
+    fi
+
+    if [ "$iwconfig_failed" = "y" ]; then
+        echo "    'iwconfig' failed to reenable TX power!"
+        echo "    Attempting direct kernel param manipulation"
+
+        local d rfk
+        for d in /sys/class/net/*; do
+            rfk=$d/device/rf_kill
+            if [ -w $rfk ]; then
+                # '1' means "turn off wifi"
+                echo "    Enabling antenna..."
+                echo 0 >>$rfk
+            fi
+        done
+    fi
+
+    # Return an appropriate status, depending on whether or not we failed to
+    # unambiguously turn the antenna on.
+    isWifiUp 
+    if [ $? -ne 1 ]; then
+        return 1
+    fi
+    #else
+    return 0
+}
+
+
+toggleWifi()
+{
+    # FIXME::[jpw;2010-11-13]
+    # Something else is running when Fn-F5 is hit, in addition to this script.
+    # Soooo... we'll let it run, then we'll ... um ... Wait.  If we then run
+    # this script after waiting, we'll _reverse_ the change.  So that won't
+    # work.
+    #
+    # The upshot is:  we have a race-condition between this script and whatever
+    # else is running.
+
+    local succeeded
+
+    if isAnyWirelessPoweredOn; then
+        turnWifiOff && succeeded=y
+    else
+        turnWifiOn && succeeded=y
+    fi
+
+    # If my own routines failed, fall back on the functoin from
+    # '/usr/share/acpi-support/state-funcs'.
+    if [ -z "$succeeded" ]; then
+        echo ""
+        echo "Still no success!  Trying to just toggle the antenna state..."
+        toggleAllWirelessStates
+    fi
+}
+
+
+############
+#
+# Main
+#
+############
 
 
 echo "" >>$LOGFILE
@@ -40,54 +219,8 @@ echo "#################### `date` ####################" \
     >>$LOGFILE
 echo "" >>$LOGFILE
 
-# FIXME::[jpw;2010-11-13]
-# Something else is running when Fn-F5 is hit, in addition to this script.
-# Soooo... we'll let it run, then we'll ... um ... Wait.  If we then run this
-# script after waiting, we'll _reverse_ the change.  So that won't work.
-#
-# The upshot is:  we have a race-condition between this script and whatever
-# else is running.
-
-local iwconfig_failed
-iwconfig_failed='n'
-if isAnyWirelessPoweredOn; then
-    echo "Found active WiFi; disabling..." >>$LOGFILE
-    iwconfig eth1 txpower off >>$LOGFILE 2>&1 || iwconfig_failed='y'
-    isAnyWirelessPoweredOn && iwconfig_failed='y'
-
-    if [ "$iwconfig_failed" = "y" ]; then
-        echo "    'iwconfig' failed to disable TX power!" >>$LOGFILE 2>&1
-        echo "    Attempting direct kernel param manipulation" \
-            >>$LOGFILE 2>&1
-
-        killWifi >>$LOGFILE 2>&1
-    fi
-else
-    echo "Enabling WiFi..." >>$LOGFILE
-    loadWifiModules >>$LOGFILE 2>&1
-
-    iwconfig eth1 txpower on >>$LOGFILE 2>&1 || iwconfig_failed='y'
-    isAnyWirelessPoweredOn || iwconfig_failed='y'
-
-    if [ "$iwconfig_failed" = "y" ]; then
-        echo "    'iwconfig' failed to reenable TX power!" >>$LOGFILE 2>&1
-        echo "    Attempting direct kernel param manipulation" \
-            >>$LOGFILE 2>&1
-
-        local d rfk
-        for d in /sys/class/net/*; do
-            rfk=$d/device/rf_kill
-            if [ -w $rfk ]; then
-                # '1' means "turn it off"
-                if [ "`cat $rfk`" = "1" ]; then
-                    echo "    Disabling rf_kill..." >>$LOGFILE 2>&1
-                    echo 0 >>$rfk
-                fi
-            fi
-        done
-    fi
-fi
-toggleBluetooth
+toggleWifi >>$LOGFILE 2>&1
+toggleBluetooth >>$LOGFILE 2>&1
 
 echo "Done." >>$LOGFILE
 
