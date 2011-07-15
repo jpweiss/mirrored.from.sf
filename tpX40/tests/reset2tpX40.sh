9ac30e6f0c520b7eb624fe36b0b67042fd05800c
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2011-07-15 19:13:56 +0000

    - Fixed some comments. - Fixed numeric keypad:   + It once again produces numbers when NumLock is on and the keys are     unshifted.   + With mousekeys enabled, the KP_<num> and KP_decimal produce the     normal keypad keys instead of moving the mouse. - The "reset" script now uses the new option names.
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/xkb/trunk@2223 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

--- tpX40/tests/reset2tpX40.sh
+++ tpX40/tests/reset2tpX40.sh
@@ -1,7 +1,7 @@
 sudo rm /var/lib/xkb/server-*.xkm
 setxkbmap -v -option
 setxkbmap -v -rules thinkpadx40 -model thinkpadx40 \
-    -option altwin:fn_super \
+    -option fn:fn_super_win \
     -option numlk:invert \
     -option numlk:un_ctrl \
     -option mousekeys:alt
