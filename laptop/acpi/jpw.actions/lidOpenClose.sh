1c30cb98c42034b7895833c8ebb74c89edd87b6c
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2013-01-10 23:33:30 +0000

    - Arranged code into functions.
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/sh.scripts/trunk@2686 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

--- laptop/acpi/jpw.actions/lidOpenClose.sh
+++ laptop/acpi/jpw.actions/lidOpenClose.sh
@@ -2,6 +2,84 @@
 #
 
 
+toggle_screenblank()
+{
+    activity=expose
+    if [ -n "$*" ]; then
+        activity=activate
+    fi
+
+    for x in /tmp/.X11-unix/*; do
+        displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
+        getXuser
+        if [ x"$XAUTHORITY" != x"" ]; then
+            export DISPLAY=":$displaynum"
+            su $user -c "xset s $activity"
+        fi
+    done
+}
+
+
+toggle_vbe_dpms()
+{
+    if [ -n "$*" ]; then
+        touch /tmp/vidtest/running-vbetool_dpms_off
+        vbetool dpms off
+    else
+        touch /tmp/vidtest/running-vbetool_dpms_on
+        vbetool dpms on
+    fi
+}
+
+
+toggle_vt()
+{
+    if [ -n "$*" ]; then
+
+        fgconsole >$LASTCONSOLE_FILE
+        chvt 1
+
+        for x in /tmp/.X11-unix/*; do
+            displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
+            getXuser
+            if [ x"$XAUTHORITY" != x"" ]; then
+                export DISPLAY=":$displaynum"
+                su $user -c "xset dpms force off"
+            fi
+        done
+
+    else
+
+        lastConsole=7
+        if [ -r $LASTCONSOLE_FILE ]; then
+            lastConsole=`cat $LASTCONSOLE_FILE`
+        fi
+        chvt $lastConsole
+        if [ `CheckPolicy` = 0 ]; then exit; fi
+
+        for x in /tmp/.X11-unix/*; do
+            displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
+            getXuser
+            if [ x"$XAUTHORITY" != x"" ]; then
+                export DISPLAY=":$displaynum"
+                pidof xscreensaver >/dev/null && xscreensaver_running=y
+                grep -q off-line /proc/acpi/ac_adapter/*/state
+                if [ $? = 1 ]; then
+                    if [ -n "$xscreensaver_running" ]; then
+                        su $user -c "xscreensaver-command -unthrottle"
+                    fi
+                fi
+                if [ -n "$xscreensaver_running" ]; then
+                    su $user -c "xscreensaver-command -deactivate"
+                fi
+                su $user -c "xset dpms force on"
+            fi
+        done
+
+    fi
+}
+
+
 test -f /usr/share/acpi-support/state-funcs || exit 0
 
 . /usr/share/acpi-support/power-funcs
@@ -16,47 +94,17 @@ LASTCONSOLE_FILE=/var/local/lidClose-lastConsole
 
 grep -q closed /proc/acpi/button/lid/*/state
 if [ $? = 0 ]; then
-
-    fgconsole >$LASTCONSOLE_FILE
-    chvt 1
-
-    for x in /tmp/.X11-unix/*; do
-        displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
-        getXuser;
-        if [ x"$XAUTHORITY" != x"" ]; then
-            export DISPLAY=":$displaynum"
-            su $user -c "xset dpms force off"
-        fi
-    done
-
+    :
+    #toggle_vt "closed"
+    #toggle_vbe_dpms "closed"
+    #toggle_dpms "closed"
+    toggle_screenblank "closed"
 else
-
-    lastConsole=7
-    if [ -r $LASTCONSOLE_FILE ]; then
-        lastConsole=`cat $LASTCONSOLE_FILE`
-    fi
-    chvt $lastConsole
-    if [ `CheckPolicy` = 0 ]; then exit; fi
-
-    for x in /tmp/.X11-unix/*; do
-        displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
-        getXuser;
-        if [ x"$XAUTHORITY" != x"" ]; then
-            export DISPLAY=":$displaynum"
-            pidof xscreensaver >/dev/null && xscreensaver_running=y
-            grep -q off-line /proc/acpi/ac_adapter/*/state
-            if [ $? = 1 ]; then
-                if [ -n "$xscreensaver_running" ]; then 
-                    su $user -c "xscreensaver-command -unthrottle"
-                fi
-            fi
-            if [ -n "$xscreensaver_running" ]; then 
-                su $user -c "xscreensaver-command -deactivate"
-            fi
-            su $user -c "xset dpms force on"
-        fi
-    done
-
+    :
+    #toggle_vt
+    #toggle_vbe_dpms
+    #toggle_dpms
+    toggle_screenblank
 fi
 
 [ -x /etc/acpi/local/lid.sh.post ] && /etc/acpi/local/lid.sh.post
