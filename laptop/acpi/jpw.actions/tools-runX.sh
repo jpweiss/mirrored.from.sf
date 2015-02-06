48ba0e5088c0cabab7547dfb3eedba087a391f1f
candide <candide@92e5ae4a-8afd-0310-8b33-eaebc6dd6768>
2015-02-06 06:19:07 +0000

    - Simplified the 'hibernate' and 'sleep' scripts down to the bare necessities. - Removed all references to the '/usr/share/acpi-support' scripts, since   most of them have disappeared in 14.04lts. - Removed useless checks for scripts and useless calls to 'CheckPolicy' and   other '/usr/share/acpi-support' functions. - 'screenblanker.sh' and 'lidOpenClose.sh' now try to use either procfs   or sysfs control-files, rather than just assuming that the former will   always be there. - Added tool-functions to replace the ones from '/usr/share/acpi-support' that   we needed.   + Propagated the changes to all other functions & scripts. - 'runXCmd_allXServers' just invokes 'runXCmd' rather than using a copy of   that 'if'-block from the latter.   + 'runXCmd' modified to get the necessary variables from its arglist     instead of calling 'getXinfo' [which replaced 'getXconsole'] for it.
    git-svn-id: file:///home/candide/src/0_Local_SVN_Repos/sh.scripts/trunk@3036 92e5ae4a-8afd-0310-8b33-eaebc6dd6768

--- laptop/acpi/jpw.actions/tools-runX.sh
+++ laptop/acpi/jpw.actions/tools-runX.sh
@@ -1,6 +1,6 @@
 # !/bin/bash
 #
-# Copyright (C) 2013 by John P. Weiss
+# Copyright (C) 2013-2015 by John P. Weiss
 #
 # This package is free software; you can redistribute it and/or modify
 # it under the terms of the Artistic License, included as the file
@@ -24,14 +24,6 @@
 ############
 
 
-if type -t getXuser; then
-    :
-else
-    # 'power-funcs' defines the 'getXuser()' and 'getXconsole' functions,
-    # amongst others.
-    . /usr/share/acpi-support/power-funcs
-fi
-
 DBGLOG=/tmp/logs/acpi-debug-event.log
 
 
@@ -40,6 +32,136 @@ DBGLOG=/tmp/logs/acpi-debug-event.log
 # Functions
 #
 ############
+
+
+getXinfo_raw()
+{
+    local targDisplay="$1";
+    shift
+
+    type -t ck-list-sessions >/dev/null || return 1
+
+    ck-list-sessions | \
+        perl -n -e \
+        'BEGIN {
+             our %sessionInfo=();
+             our $curSesId="";
+             our $targDisplay="'$targDisplay'";
+         }' -e \
+        'sub dequote($) {
+             my $s = shift();
+             $s =~ s/^[\x27"]//;
+             $s =~ s/[\x27"]$//;
+            return($s);
+         }' -e \
+        'if (m/^(Session.+)$/) {
+             $curSesId=$1;
+         }
+         elsif ($curSesId ne "") {
+             m/(\S+) = (\S+)/;
+             if ($1 ne "") {
+                 $sessionInfo{$curSesId}{$1} = dequote($2);
+             }
+         }'  -e \
+        'END {
+             my $curX11Disp="";
+             my $x_display="";
+             my $consoleDev="";
+             my $uid="";
+             foreach my $k (sort(keys(%sessionInfo))) {
+                 $curX11Disp = $sessionInfo{$k}{"x11-display"};
+                 next unless ( ($sessionInfo{$k}{"active"} eq "TRUE")
+                               ||
+                               ( ($targDisplay ne "")
+                                 &&
+                                 ($targDisplay eq $curX11Disp)
+                                 )
+                              );
+
+                 $uid = $sessionInfo{$k}{"unix-user"};
+                 $x_display = $curX11Disp;
+                 $consoleDev = $sessionInfo{$k}{"x11-display-device"};
+                 if ($consoleDev eq "") {
+                     $consoleDev = $sessionInfo{$k}{"display-device"};
+                 }
+             }
+
+             print "\"$uid\" \"$x_display\" \"$consoleDev\"\n";
+         }'
+}
+
+
+getUserInfo()
+{
+    # Note the jiggery-pokery taking place to trim the quotes off the arg.
+    local uid="${1%\"}"
+    shift
+
+    # Format of an entry in /etc/passwd:
+    #     <usernm>:<passwd>:<uid>:<gid>:<realname>:<home>:<shell>
+    perl -a -F: -n -e \
+        'BEGIN {
+             our $userId="'${uid#\"}'";
+         }' -e \
+        'if ($F[2] eq $userId) {
+             print "\"$F[0]\" \"$F[5]\"\n";
+         }' \
+             /etc/passwd
+}
+
+
+getXinfo()
+{
+    # Everything comes from 'getXinfo_raw' and 'getUserInfo' quoted, because
+    # it's being written out by an embedded perl script.
+    #
+    # We don't need that quoting from within the shell, so remove it.
+
+    set -- `getXinfo_raw`
+    local uid="${1%\"}"
+    shift
+    local xdisplay="${1%\"}"
+    shift
+    local consoleDev="${1%\"}"
+    shift
+
+    local consoleNum="${consoleDev##*/}"
+    consoleNum="${consoleNum#tty}"
+    consoleNum="${consoleNum#vcs}"
+    consoleNum="${consoleNum#vcsa}"
+
+    set -- `getUserInfo $uid`
+
+    local userName="${1%\"}"
+    shift
+    local xauthfile="${1%\"}/.Xauthority"
+    shift
+
+    echo -n "${userName#\"} ${uid#\"} ${xdisplay#\"} "
+    echo "${xauthfile#\"} ${consoleNum#\"} ${consoleDev#\"}"
+}
+
+
+extractUsername_fromXInfo()
+{
+    local xinfo="$1"
+    if [ $# -lt 2 ]; then
+        set -- $xinfo
+    fi
+
+    echo "$1"
+}
+
+
+extractXAuth_fromXInfo()
+{
+    local xinfo="$1"
+    if [ $# -lt 2 ]; then
+        set -- $xinfo
+    fi
+
+    echo "$4"
+}
 
 
 wrapper__runX()
@@ -51,31 +173,45 @@ wrapper__runX()
     local xauth="$1"
     shift
 
-    if [ -z "$DISPLAY" ]; then
-        DISPLAY="$xdisplay"
-    fi
-    export DISPLAY
-
-    if [ -z "$XAUTHORITY" ]; then
-        XAUTHORITY="$xauth"
-    fi
-    export XAUTHORITY
-
     if [ -z "$xuser" ]; then
         return 1
     fi
+
+    local oldDISPLAY="$DISPLAY"
+    export DISPLAY="$xdisplay"
+    local oldXAUTHORITY="$XAUTHORITY"
+    export XAUTHORITY="$xauth"
 
     for cmd in "$@"; do
         # DBG:  Comment out when not in use.
         #echo "wrapper__runX:  su $xuser -c \"$cmd\"" >>$DBGLOG
         su $xuser -c "$cmd"
     done
+
+    export DISPLAY="$oldDISPLAY"
+    export XAUTHORITY="$oldXAUTHORITY"
 }
 
 
 runXCmd()
 {
-    getXconsole
+    local user xdisplay xauth
+    if [ "$1" = "--has-xinfo" ]; then
+        shift
+
+        xdisplay="$1"
+        shift
+        user="$1"
+        shift
+        xauth="$1"
+        shift
+    else
+        set -- `getXinfo`
+        user="$1"
+        xdisplay="$3"
+        xauth="$4"
+    fi
+
     if [ -z "$user" ]; then
         return 1
     fi
@@ -86,32 +222,43 @@ runXCmd()
         shift
         # DBG:  Comment out when not in use.
         #echo "runXCmd:  \"$cmd_fn $@\"" >>$DBGLOG
-        $cmd_fn  "$DISPLAY" "$user" "$XAUTHORITY" "$@"
+        $cmd_fn  "$xdisplay" "$user" "$xauth" "$@"
     else
-        wrapper__runX "$DISPLAY" "$user" "$XAUTHORITY" "$@"
+        wrapper__runX "$xdisplay" "$user" "$xauth" "$@"
     fi
 }
 
 
 runXCmd_allXServers()
 {
+    local user xinfo xauth
+    local x cmd_fn
+
     for x in /tmp/.X11-unix/*; do
         displaynum=`echo $x | sed s#/tmp/.X11-unix/X##`
-        getXuser
+
+        # We can't use 'set' here, since we need to preserve the current
+        # arglist.  Note that "$xinfo" will have its elements already quoted,
+        # so there's no need to put quotes around _it_.
+        xinfo=`getXinfo ":$displaynum"`
+        user=`extractUsername_fromXInfo $xinfo`
+        xauth=`extractXAuth_fromXInfo $xinfo`
+
         if [ -z "$user" ]; then
             continue
         fi
 
-        if [ "$1" = "-f" ]; then
-            shift
-            cmd_fn="$1"
-            shift
-            # DBG:  Comment out when not in use.
-            #echo "runXCmd_allXServers:  \"$cmd_fn $@\"" >>$DBGLOG
-            $cmd_fn  ":$displaynum" "$user" "$XAUTHORITY" "$@"
-        else
-            wrapper__runX ":$displaynum" "$user" "$XAUTHORITY" "$@"
-        fi
+        # if [ "$1" = "-f" ]; then
+        #     shift
+        #     cmd_fn="$1"
+        #     shift
+        #     # DBG:  Comment out when not in use.
+        #     #echo "runXCmd_allXServers:  \"$cmd_fn $@\"" >>$DBGLOG
+        #     $cmd_fn ":$displaynum" "$user" "$xauth" "$@"
+        # else
+        #     wrapper__runX ":$displaynum" "$user" "$xauth" "$@"
+        # fi
+        runXCmd --has-xinfo ":$displaynum" "$user" "$xauth" "$@"
     done
 }
 
